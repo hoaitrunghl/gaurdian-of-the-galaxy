57c643690aab485d32de013720d3ba06f2a55fb4
thuanchat294@gmail.com <thuanchat294@gmail.com@ed22445d-1d62-aa1a-f75f-9fccddb1f91f>
2014-10-03 10:53:33 +0000

    [Update] Move Meteors (Ramdom and Repeat)

--- Source/GaurdianOfTheGalaxy/src/uit/gaurdianofthegalaxy/MainActivity.java
+++ Source/GaurdianOfTheGalaxy/src/uit/gaurdianofthegalaxy/MainActivity.java
@@ -53,6 +53,8 @@ public class MainActivity extends BaseGameActivity {
  //Khai báo biến Meteors.
 private Texture mTextureMeteors;
 private TextureRegion mTextureRegionMeteors;
+private LinkedList targetLLMeteors;
+private LinkedList TargetsToBeAddedMeteors;
  //Khao báo biến music
  private Music backgroundMusic;
 
@@ -76,25 +78,29 @@ private TextureRegion mTextureRegionMeteors;
 
  @Override
  public void onLoadResources() {
- 
-  
+	 
+	
+  //====================================== Load Background ===========================================================//
   this.mAutoParallaxBackgroundTexture = new Texture(2048, 2048, TextureOptions.DEFAULT); 
   this.mParallaxLayerBack = TextureRegionFactory.createFromAsset(this.mAutoParallaxBackgroundTexture
     , this, "gfx/background1.jpg", 0, 0);
   this.mEngine.getTextureManager().loadTextures( this.mAutoParallaxBackgroundTexture);
   
-    TextureRegionFactory.setAssetBasePath("gfx/");
-    // load sprite animate
-    this.mTextureAirplanet = new Texture(1024, 512, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+   //=====================================  Load sprite animate ===================================================//
+  	TextureRegionFactory.setAssetBasePath("gfx/");
+  	this.mTextureAirplanet = new Texture(1024, 512, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
     this.mTextureRegionAirplanet = TextureRegionFactory.createTiledFromAsset(this.mTextureAirplanet, this, "airplanet.png",0, 0, 4, 4);
     this.mEngine.getTextureManager().loadTexture(this.mTextureAirplanet);
-    //Load sprite
+   
+    //====================================== Load sprite Planets ==================================================//
     this.mTexturePlanets = new Texture(256, 256,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
     mTextureRegionPlanets = TextureRegionFactory.createFromAsset(this.mTexturePlanets, this, "planets1.png", 0, 0);
     this.mEngine.getTextureManager().loadTexture(this.mTexturePlanets);
-    // Load Meteors
+    
+    //====================================== Load Meteors ========================================================//
     this.mTextureMeteors = new Texture(512, 512,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
     mTextureRegionMeteors = TextureRegionFactory.createFromAsset(this.mTextureMeteors, this, "meteors1.png", 0, 0);
+    //this.mTextureRegionMeteors = TextureRegionFactory.createTiledFromAsset(this.mTextureMeteors, this, "meteors1.png", 0, 0, 1, 1);
     this.mEngine.getTextureManager().loadTexture(this.mTextureMeteors);
         
     /////Music
@@ -113,37 +119,58 @@ private TextureRegion mTextureRegionMeteors;
  @Override
  public Scene onLoadScene() {
   
-	 ///Load background
+	 ///===================================*Load background*=============================================//
   this.mEngine.registerUpdateHandler(new FPSLogger());
   final Scene scene = new Scene();
   final AutoParallaxBackground autoParallaxBackground = new AutoParallaxBackground(0, 0, 0, 5);
-  
   autoParallaxBackground.attachParallaxEntity(new ParallaxEntity(-5.0f, new Sprite(0, CAMERA_HEIGHT - 
     this.mParallaxLayerBack.getHeight()
     , this.mParallaxLayerBack)));
-  //Load sprite animate
   
-  scene.setBackground(autoParallaxBackground);
+  //========================================*Load sprite animate*========================================//
+    scene.setBackground(autoParallaxBackground);
   final AnimatedSprite animatedSprite =  new AnimatedSprite(0, CAMERA_HEIGHT/2, mTextureRegionAirplanet);
   scene.attachChild(animatedSprite);
   animatedSprite.setScale((float) 1);// tăng kích thước sprite x lần
   //animatedSprite.animate(new long[] { 200, 200, 200 },0,2, true);
-  //Load sprite
+  
+  //========================================*Load sprite Planets*=========================================//
   final Sprite SpritePlanets =  new Sprite(CAMERA_WIDTH, CAMERA_HEIGHT/2, mTextureRegionPlanets);
   scene.attachChild(SpritePlanets);
   SpritePlanets.setScale(1);
   //Move sprite Planets
-  MoveXModifier modifier = new MoveXModifier(80, (float) (CAMERA_WIDTH), 0-SpritePlanets.getX());
-  SpritePlanets.registerEntityModifier(modifier);
-  //SpritePlanets.setPosition(pX, pY)
-  /// Meteors
-  final Sprite SpriteMeteors =  new Sprite((float) (CAMERA_WIDTH/1.5), CAMERA_HEIGHT/8, mTextureRegionMeteors);
-  scene.attachChild(SpriteMeteors);
-  SpriteMeteors.setScale((float) (0.2));
-  //Load music
-  this.backgroundMusic.play();
-  
-  
+  MoveXModifier modifierPlanets = new MoveXModifier(80, (float) (CAMERA_WIDTH), 0-SpritePlanets.getX());
+  SpritePlanets.registerEntityModifier(modifierPlanets);
+ 
+  //=================================== * Meteors and move * ===========================================================//
+  TimerHandler spriteTimerHandler;
+  float mEffectSpawnDelay = 2f;
+  spriteTimerHandler = new TimerHandler(mEffectSpawnDelay, true, new ITimerCallback() {
+	  public void onTimePassed(TimerHandler pTimerHandler) {
+		  Random rand = new Random();
+		  int minY = 5;
+		  int maxY = CAMERA_HEIGHT - mTextureRegionMeteors.getWidth();
+		  int rangeY = maxY - minY;
+		  int y = rand.nextInt(rangeY) + minY;
+		  double x = CAMERA_WIDTH / 1.2;
+		  targetLLMeteors = new LinkedList();
+		  TargetsToBeAddedMeteors = new LinkedList();
+		  //AnimatedSprite target = new AnimatedSprite(x, y, this.mTextureRegionMeteors);
+		  //target.animate(new long[] { 200, 200, 200 }, 0, 2, true);
+		  final Sprite SpriteMeteors =  new Sprite((float) x, y, mTextureRegionMeteors);
+		  scene.attachChild(SpriteMeteors);
+		  SpriteMeteors.setScale((float) (0.2));
+		  int minDuration = 3;// tốc độ vật bay chậm nhất
+		  int maxDuration = 5;// tốc độ vật bay nhanh nhất
+		  int rangeDuration = maxDuration - minDuration;
+		  int actualDuration = rand.nextInt(rangeDuration) + minDuration;
+		  MoveXModifier mod = new MoveXModifier(actualDuration, SpriteMeteors.getX(), 0-SpriteMeteors.getX());
+		  SpriteMeteors.registerEntityModifier(mod);
+		  TargetsToBeAddedMeteors.add(SpriteMeteors);}});
+  	getEngine().registerUpdateHandler(spriteTimerHandler);
+  	
+  //==============================================*Load music*================================================//
+  this.backgroundMusic.play(); 
   return scene;
  }
 
